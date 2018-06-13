4c7b18eacf52e57f4ca486c0afca658599c9c7c1
3dek <x0ffma@gmail.com>
2018-06-13 13:38:55 +0200

    chore: purpose of demo keys

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AlgoliaSearchInput from './AlgoliaSearchInput';
 
+// DEMO ACCOUNT ONLY, SET YOU OWN KEYS TO TRY WITH YOU INDEXES 
 const APP_ID = '393BU8WBBH';
 const SEARCH_API_KEY = '860bd6d75eafc6aee108f3d23a50ced1';
 
+// example index conf, keys are algolia indexes
 const indexes = {
     'nba-teams': {
         label: 'Teams',
